0a06b19d54743436d81107fc0c3bed3cee9e6137
codemonkeh <14917019+codemonkeh@users.noreply.github.com>
2018-06-25 17:02:21 +1000

    Added basic (untested) implementation of S3 file upload

--- Lambda/src/Lambda/Services/IS3FileService.cs
+++ Lambda/src/Lambda/Services/IS3FileService.cs
@@ -1,6 +1,9 @@
-﻿namespace Lambda.Services
+﻿using System.Threading.Tasks;
+
+namespace Lambda.Services
 {
     public interface IS3FileService
     {
+        Task UploadFile(string filePath, string bucketName, string keyName, string regionSystemName);
     }
 }
--- Lambda/src/Lambda/Services/S3FileService.cs
+++ Lambda/src/Lambda/Services/S3FileService.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Amazon;
+using Amazon.S3;
+using Amazon.S3.Transfer;
 
 namespace Lambda.Services
 {
     // need to install nuget packages:
-    // * AWSSDK.Extensions.NETCore.Setup (and perhaps AWSSDK.S3?)
+    // * AWSSDK.S3
+    // * AWSSDK.Extensions.NETCore.Setup
     public class S3FileService : IS3FileService
     {
         private readonly ILoggingService _logger;
@@ -17,12 +20,44 @@ namespace Lambda.Services
             _logger = logger;
         }
 
-        public async Task UploadFile(string filePath, string bucketName, string keyName, RegionEndpoint endpoint)
+        public async Task UploadFile(string filePath, string bucketName, string keyName, string regionSystemName)
         {
-            //var bucketRegion = RegionEndpoint.APSoutheast2; //Sydney
-            //var s3Client = new AmazonS3Client(bucketRegion);
-            //var fileTransferUtility = new TransferUtility(s3Client);
-            //await fileTransferUtility.UploadAsync(filePath, bucketName, keyName);
+            if (filePath == null) throw new ArgumentNullException(nameof(filePath));
+            if (bucketName == null) throw new ArgumentNullException(nameof(bucketName));
+            if (keyName == null) throw new ArgumentNullException(nameof(keyName));
+            if (regionSystemName == null) throw new ArgumentNullException(nameof(regionSystemName));
+
+            var endPoint = RegionEndpoint.GetBySystemName(regionSystemName);
+            if (endPoint == null) throw new Exception($"Endpoint with system name \"{regionSystemName}\" could not be resolved.");
+
+            await UploadFile(filePath, bucketName, keyName, endPoint);
+        }
+
+        //todo: test
+        //var bucketRegion = RegionEndpoint.APSoutheast2; //Sydney
+        public async Task UploadFile(string filePath, string bucketName, string keyName, RegionEndpoint bucketRegion)
+        {
+            if (filePath == null) throw new ArgumentNullException(nameof(filePath));
+            if (bucketName == null) throw new ArgumentNullException(nameof(bucketName));
+            if (keyName == null) throw new ArgumentNullException(nameof(keyName));
+            if (bucketRegion == null) throw new ArgumentNullException(nameof(bucketRegion));
+
+            try
+            {
+                _logger.Log($"Attempting to upload file to S3 with local path \"{filePath}\"...");
+
+                // the client should probably be static if this function was heavily used
+                using (var s3Client = new AmazonS3Client(bucketRegion))
+                using (var fileTransferUtility = new TransferUtility(s3Client))
+                    await fileTransferUtility.UploadAsync(filePath, bucketName, keyName);
+
+                _logger.Log($"File upload complete.");
+            }
+            catch (AmazonS3Exception ex)
+            {
+                _logger.LogError($"Error uploading file with path \"{filePath}\"", ex);
+                throw;
+            }            
         }
     }
 }
